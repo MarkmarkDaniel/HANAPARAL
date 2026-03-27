00718be92dc76544cf99d61ebf324869f3ce1cd2
Hannah Mae Sasi <margaux080824@gmail.com>
2026-03-27 10:59:34 +0800

    feat: implement singleton pattern for RemoteConfigRepository

--- app/src/main/java/com/example/hanaparal/data/repository/RemoteConfigRepository.kt
+++ app/src/main/java/com/example/hanaparal/data/repository/RemoteConfigRepository.kt
@@ -4,4 +4,13 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 
 class RemoteConfigRepository private constructor() {
     private val remoteConfig = FirebaseRemoteConfig.getInstance()
+
+    companion object {
+        @Volatile
+        private var instance: RemoteConfigRepository? = null
+
+        fun get(): RemoteConfigRepository = instance ?: synchronized(this) {
+            instance ?: RemoteConfigRepository().also { instance = it }
+        }
+    }
 }
